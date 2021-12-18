b194cb39bdd21a3e84a0322b21bae6bc9eb4e44c
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-18 19:49:53 +0100

    _

--- src/svg.tsx
+++ src/svg.tsx
@@ -3,3 +3,17 @@ import React from 'react'
 
 
 
+const Instagram = () =>{
+    return (<></>)
+}
+
+
+const Twitter = () =>{
+    return (<></>)
+}
+const Github = () =>{
+    return (<></>)
+}
+const Linkedin = () =>{
+    return (<></>)
+}
