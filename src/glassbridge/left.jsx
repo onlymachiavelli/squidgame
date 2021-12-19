f366a72625dd1f4a4468d88a7bb251bb1c1f2801
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-19 23:40:27 +0100

    t to j

--- src/glassbridge/left.jsx
+++ src/glassbridge/left.jsx
@@ -2,12 +2,17 @@ import React from "react"
 import logo from "./../../public/img/logo.png"
 import Image from "next/image"
 import styles from "./../../styles/GlassBridge.module.css"
+import { GentleMan } from "./../svg"
 const LeftSide = () => {
   return (
     <div className={styles.inFlex}>
       <div className={styles.logo}>
         <Image src={logo} />
         <p id={styles.gameTitle}>Glass Bridge</p>
+      </div>
+      <div className={styles.msg}>
+        <p>If You Lose , You're Racist</p>
+        <GentleMan Width="300px" Height="200px" />
       </div>
     </div>
   )
