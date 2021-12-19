9800a1c809f65d45ddb9bc278387749a050b0799
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-19 22:40:55 +0100

    fcking some shiits up

--- pages/glassbridge.jsx
+++ pages/glassbridge.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import styles from "./../styles/GlassBridge.module.css"
-import LeftSide from "./../src/GameBridge/UI/Left"
+//import { LeftSide } from "./../src/GameBridge/UI/Left"
+import { shit } from "./../src/GameBridge/UI/Left"
+
 const BridgeGame = () => {
   return (
     <main className={styles.container}>
-      <LeftSide />
+      <shit />
     </main>
   )
 }
--- src/GameBridge/UI/Left.tsx
+++
@@ -1,8 +0,0 @@
-import React, {} from 'react'
-
-
-const LeftSide = () => {
-    return <div >this is left side </div>
-}
-
-export default LeftSide
