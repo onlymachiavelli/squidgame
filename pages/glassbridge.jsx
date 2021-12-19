901cd7dd3b9a2d1295e638eb8296beaf4b8a8335
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-19 23:31:15 +0100

    styling

--- pages/glassbridge.jsx
+++ pages/glassbridge.jsx
@@ -1,13 +1,12 @@
 import React from "react"
 import styles from "./../styles/GlassBridge.module.css"
 import Image from "next/image"
-import logo from "./../public/img/logo.png"
+
 import LeftSide from "../src/glassbridge/left"
 const BridgeGame = () => {
   return (
     <main className={styles.container}>
       <div className={styles.logo}>
-        <Image src={logo} width={"100"} height={"50"} />
         <LeftSide />
       </div>
     </main>
--- src/glassbridge/left.jsx
+++ src/glassbridge/left.jsx
@@ -1,7 +1,15 @@
 import React from "react"
-
+import logo from "./../../public/img/logo.png"
+import Image from "next/image"
+import styles from "./../../styles/GlassBridge.module.css"
 const LeftSide = () => {
-  return <>Hello world</>
+  return (
+    <div className={styles.inFlex}>
+      <div className={styles.logo}>
+        <Image src={logo} />
+      </div>
+    </div>
+  )
 }
 
 export default LeftSide
