d389f2a9f1c83a52f327585b3f5f4ec14f601156
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-19 22:49:10 +0100

    fixing stuffs

--- pages/glassbridge.jsx
+++ pages/glassbridge.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import styles from "./../styles/GlassBridge.module.css"
-//import { LeftSide } from "./../src/GameBridge/UI/Left"
-import { shit } from "./../src/GameBridge/UI/Left"
-
+import Image from "next/image"
+import logo from "./../public/img/logo.png"
+import LeftSide from "../src/glassbridge/left"
 const BridgeGame = () => {
   return (
     <main className={styles.container}>
-      <shit />
+      <div className={styles.logo}>
+        <Image src={logo} width={"100"} height={"50"} />
+        <LeftSide />
+      </div>
     </main>
   )
 }
---
+++ src/glassbridge/left.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const LeftSide = () => {
+  return <>Hello world</>
+}
+
+export default LeftSide
