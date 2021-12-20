5b994c7b24d05155ddbd94ca835c9d43b5dc7cd9
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-20 01:25:52 +0100

    svg's

--- src/glassbridge/Right.jsx
+++ src/glassbridge/Right.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import styles from "./../../styles/GlassBridge.module.css"
-
+import { Coffin } from "./../svg"
 const Right = () => {
   return (
     <div className={styles.right}>
       <p id={styles.getin}>Get In Mother Fcker</p>
+      <Coffin />
     </div>
   )
 }
