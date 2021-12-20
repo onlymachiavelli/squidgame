4c56e6bf39aa642e5b481c7cc2113dca1220b5cf
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-20 01:09:05 +0100

    assemble

--- pages/glassbridge.jsx
+++ pages/glassbridge.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 import styles from "./../styles/GlassBridge.module.css"
 import Image from "next/image"
-
+import Bridge from "../src/glassbridge/Bridge"
 import LeftSide from "../src/glassbridge/left"
 const BridgeGame = () => {
   return (
     <main className={styles.container}>
       <LeftSide />
-      <div>hello world</div>
+      <Bridge />
       <div>tst reserved </div>
     </main>
   )
--- src/glassbridge/Bridge.jsx
+++ src/glassbridge/Bridge.jsx
@@ -1,8 +1,8 @@
 import React from "react"
-import styles from "./../../styles/GlassBridge.css"
+import styles from "./../../styles/GlassBridge.module.css"
 import { Flag } from "./../svg"
 const Bridge = () => {
-  return <div className={styles.bridge}></div>
+  return <div className={styles.bridge}>hello rld</div>
 }
 
 export default Bridge
