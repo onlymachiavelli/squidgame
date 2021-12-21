751f467202b46634e2bf185b1b185c55b9abfd0a
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-21 23:36:49 +0100

    randomize

---
+++ src/glassbridge/Button.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react"
+
+import styles from "./../../styles/GlassBridge.module.css"
+
+useEffect(() => {
+  console.log(Math.floor(Math.random() * 3))
+}, [])
+const Button = () => {
+  return <></>
+}
